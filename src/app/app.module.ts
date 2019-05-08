846366240e2ec8901e0c7b6aa164419779d3c72a
AhmedRabi <ahmedrabi100@gmail.com>
2019-05-08 11:48:34 +0200

    Routing and Components
    Added Navbar
    all the components
    defined the Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { HomeComponent } from "./home/home.component";
+import { ExpertiseComponent } from "./expertise/expertise.component";
+import { JoinUsComponent } from "./join-us/join-us.component";
+import { ContactComponent } from "./contact/contact.component";
+import { Routes, RouterModule } from "@angular/router";
+import { NotFoundComponent } from "./not-found/not-found.component";
+
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "ourteam", redirectTo: "home", pathMatch: "full" },
+  { path: "expertise", component: ExpertiseComponent },
+  { path: "joinus", component: JoinUsComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "**", component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    ExpertiseComponent,
+    JoinUsComponent,
+    ContactComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
